4deda92168b767ed31f75098b91c33c5a1fc3181
mateuskuritza <mateuskuritza@gmail.com>
2021-12-07 16:36:09 -0300

    Preparando revisão de código

--- src/TopArea/MobileTopBar.js
+++
@@ -1,9 +0,0 @@
-export default function MobileTopBar() {
-  return (
-    <div className="mobile-top-bar">
-      <ion-icon name="logo-instagram"></ion-icon>
-      <img src="images/logo.png" alt="Logo do Instagram não encontrada." />
-      <ion-icon name="paper-plane-outline"></ion-icon>
-    </div>
-  );
-}
